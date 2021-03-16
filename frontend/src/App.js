80a74bd19e315fc7f26025225db169e412fa8135
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-03-16 09:36:15 +0900

    send reserve for zoom mtg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
 function App() {
+
+  const postReserve = () => {
+    axios.post("/reserve/zoom/mtg").then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="reserve-btn" onClick={postReserve}>
+        新規予約
+      </div>
     </div>
   );
 }
